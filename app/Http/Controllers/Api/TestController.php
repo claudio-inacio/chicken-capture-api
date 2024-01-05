cea0a099523b7165b5a79761a5198b4051cac0a1
lohanmatheus <lohanmatheus.dev@gmail.com>
2024-01-05 16:13:56 -0300

    first commit

---
+++ app/Http/Controllers/Api/TestController.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Http\Controllers\Api\Proposal;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+set_time_limit(0);
+ini_set('memory_limit', -1);
+
+class TestController extends Controller
+{
+
+    public function test(Request $request){}
+}
